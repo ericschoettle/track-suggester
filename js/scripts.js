35bbfe135c7989421723f2bb46072986dc1828ce
ericschoettle <eric.schoettle@gmail.com>
2017-03-24 15:33:43 -0700

    add scroll to functionality

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,10 @@ function totals (language, question1, question2, question3, question4, question5
  // Can I do this with for loops and arrays?
 
 // front end logic
+function scrollTo(hash) {
+    location.hash = "#" + hash;
+} // not sure if this is front or back end.
+
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
     event.preventDefault();
@@ -63,6 +67,7 @@ $(document).ready(function() {
     if (fuckYouTotal >= 2) {
       alert("go fuck yourself");
     } else if (sharpTotal >= 3) {
+      scrollTo("sharp")
       $("#sharp .content").prepend("<p>C# would be a good choice for you, " + name + ". Learn a bit more about C#</p>");
       if (javaTotal >= 2) {
         $("#sharp .content").append("<p>You should also take a look at <a href='#java'>Java.</a></p>");
@@ -74,6 +79,7 @@ $(document).ready(function() {
         $("#sharp .content").append("<p>You should also take a look at <a href='#other'>other great things to do in life.</a></p>");
       }
     } else if (javaTotal >= 3) {
+      scrollTo("java")
       $("#java .content").prepend("<p>Java would be a good choice for you, " + name + ". Learn a bit more about Java!</p>");
       if (sharpTotal >= 2) {
         $("#java .content").append("<p>You should also take a look at <a sharp='C#a'>Java.</a></p>");
@@ -85,6 +91,7 @@ $(document).ready(function() {
         $("#java .content").append("<p>You should also take a look at <a href='#other'>other great things to do in life.</a></p>");
       }
     } else if (phpTotal >= 3) {
+      scrollTo("php")
       $("#php .content").prepend("<p>PHP would be a good choice for you, " + name + ". Learn a bit more about PHP!</p>");
       if (javaTotal >= 2) {
         $("#php .content").append("<p>You should also take a look at <a href='#java'>Java.</a></p>");
@@ -96,6 +103,7 @@ $(document).ready(function() {
         $("#php .content").append("<p>You should also take a look at <a href='#other'>other great things to do in life.</a></p>");
       }
     } else if (rubyTotal >= 3) {
+      scrollTo("ruby")
       $("#ruby .content").prepend("<p>Ruby would be a good choice for you, " + name + ". Learn a bit more about Ruby!</p>");
       if (javaTotal >= 2) {
         $("#ruby .content").append("<p>You should also take a look at <a href='#java'>Java.</a></p>");
@@ -107,6 +115,7 @@ $(document).ready(function() {
         $("#ruby .content").append("<p>You should also take a look at <a href='#other'>other great things to do in life.</a></p>");
       }
     } else if (otherTotal >= 3) {
+      scrollTo("other")
       $("#other .content").prepend("<p>Other things be a good choice for you, " + name + ". This isn't to say you wouldn't be great for programming - odds are, programming could use a few more artistic, people oriented, wonderful people like yourself. Just know that it sounds like you might not need programming as much as it needs you</p>");
       if (javaTotal >= 2) {
         $("#other .content").append("<p>You should also take a look at <a href='#java'>Java.</a></p>");
@@ -118,7 +127,7 @@ $(document).ready(function() {
         $("#other .content").append("<p>You should also take a look at <a href='#sharp'>C#.</a></p>");
       }
     } else {
-      alert ("you're quite the polymath! Pick your own path!");
+      alert ("You're quite the polymath! Pick your own path!");
     }
   });
 });
