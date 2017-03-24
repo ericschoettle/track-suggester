16352275044b05714155070c27916ef1f973e107
ericschoettle <eric.schoettle@gmail.com>
2017-03-24 10:22:07 -0700

    troubleshoot back end logic and fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 // back end logic
-function totals (language, question1, question2, question3, question4, question5) {
+function totals (language, question1, question2) {
+// function totals (language, question1, question2, question3, question4, question5) {
   var languageTotal = 0;
   if (question1 === language) {
-    var languageTotal += 1;
+    var languageTotal = languageTotal + 1;
   }
   if (question2 === language) {
-    var languageTotal += 1;
+    var languageTotal = languageTotal + 1;
   }
-  if (question2 === language) {
-    var languageTotal += 1;
-  }
-  if (question2 === language) {
-    var languageTotal += 1;
-  }
-  if (question2 === language) {
-    var languageTotal += 1;
-  }
-  if (question2 === language) {
-    var languageTotal += 1;
+  // if (question2 === language) {
+  //   var languageTotal += 1;
+  // }
+  // if (question2 === language) {
+  //   var languageTotal += 1;
+  // }
+  // if (question2 === language) {
+  //   var languageTotal += 1;
+  // }
+  // if (question2 === language) {
+  //   var languageTotal += 1;
 
   return languageTotal
-  }
 }
 
  // questions - do I want global or local variables? Can I do this in a more compact way?
@@ -38,12 +38,17 @@ $(document).ready(function() {
     var dob = $("input#date").val();
 
     sharpTotal = totals("sharp", executive, activity);
+    console.log(sharpTotal);
     javaTotal = totals("java", executive, activity);
+    console.log(javaTotal);
     phpTotal = totals("php", executive, activity);
+    console.log(phpTotal);
     rubyTotal = totals("ruby", executive, activity);
+    console.log(rubyTotal);
     otherTotal = totals("other", executive, activity);
+    console.log(otherTotal);
     fuckYouTotal = totals("fuck-you", executive, activity);
-
+    console.log(fuckYouTotal);
 
   });
 });
