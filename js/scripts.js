46709d9ae2ed62d4cecad245e1baa88b67b7584e
ericschoettle <eric.schoettle@gmail.com>
2017-03-24 10:14:19 -0700

    build back-end logic to add up responses and front end procedure to call back end functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+// back end logic
+function totals (language, question1, question2, question3, question4, question5) {
+  var languageTotal = 0;
+  if (question1 === language) {
+    var languageTotal += 1;
+  }
+  if (question2 === language) {
+    var languageTotal += 1;
+  }
+  if (question2 === language) {
+    var languageTotal += 1;
+  }
+  if (question2 === language) {
+    var languageTotal += 1;
+  }
+  if (question2 === language) {
+    var languageTotal += 1;
+  }
+  if (question2 === language) {
+    var languageTotal += 1;
+
+  return languageTotal
+  }
+}
+
+ // questions - do I want global or local variables? Can I do this in a more compact way?
+
+// front end logic
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
     event.preventDefault();
@@ -8,6 +36,14 @@ $(document).ready(function() {
     var activity = $("input:radio[name=activity]:checked").val();
 
     var dob = $("input#date").val();
-    console.log(dob);
+
+    sharpTotal = totals("sharp", executive, activity);
+    javaTotal = totals("java", executive, activity);
+    phpTotal = totals("php", executive, activity);
+    rubyTotal = totals("ruby", executive, activity);
+    otherTotal = totals("other", executive, activity);
+    fuckYouTotal = totals("fuck-you", executive, activity);
+
+
   });
 });
